607a467b5bb1ffc38c16c52a86de716680808914
kayra ozerk <kayra.ozerk@sabanciuniv.edu>
2024-11-25 21:35:11 +0300

    cakma login ekranı baslangici

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Sabanci University</title>
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="assets/css/icons/icomoon/styles.css">
+    
+</head>
+<body>
+    <div class="header-container">
+        <!-- University title -->
+        <img src="assets/images/screenshots/sabancilogo.png">
+        <h1>VOTE & CHOOSE YOUR FAVORITE</h1>
+        <h2>Click to View the Rules</h2>
+
+        <!-- Rules button -->
+        <button class="rules-button">
+            ⬇️ View the Rules ⬇️
+        </button>
+
+        <!-- Nominate and Login buttons -->
+        <div class="action-buttons">
+            <button class="nominate-button">Nominate</button>
+            <button class="login-button">Login</button>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <p class="footer-text">YOU CAN VOTE BETWEEN START_DATE - END_DATE</p>
+</body>
+</html>
