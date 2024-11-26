7270e13aebb29f201620d20ecd938f76b68b751c
Damla Aydın <aydin.damla02@gmail.com>
2024-11-26 13:35:30 +0300

    the layout fixed in vote screen

--- voteScreen.php
+++ voteScreen.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Teaching Awards - Sabancı University</title>
 
@@ -33,11 +32,27 @@
 
 
 
-	<style>
+	<style>		
+		
+		.header {
+			display: flex;
+			align-items: center;
+			justify-content: flex-start;
+			gap: 15px;
+			margin-bottom: 20px;
+		}
+		.header img {
+			height: 70px;
+			width: auto;
+		}
+		.header h3 {
+			margin: 0;
+			font-size: 1.5rem;
+			font-weight: bold;
+		}
 		.container {
-			max-width: 1200px;
-			margin: auto;
-			padding-top: 30px;
+			max-width: 1400px;
+			padding-top: 10px;
 		}
 		.card {
 			text-align: center;
@@ -65,7 +80,9 @@
 			text-align: center;
 			font-size: 1.5em;
 			font-weight: 500;
-			background-color: #eee;
+			background-color: #3f51b5; /* Indigo blue */
+    		color: white; 
+    		opacity: 0.75; 
 			padding: 10px;
 			border-radius: 10px;
 			margin-bottom: 30px;
@@ -75,9 +92,9 @@
 <body>
 	<div class="container">
 		<!-- Header -->
-		<div class="d-flex justify-content-between align-items-center mb-4">
-			<img src="https://yabangee.com/wp-content/uploads/sabancı-university-2.jpg" alt="Sabancı University Logo" style="height: 60px;">
-			<h3>Teaching Awards</h3>
+		<div class="header">
+			<img src="https://yabangee.com/wp-content/uploads/sabancı-university-2.jpg" alt="Sabancı University Logo">
+			<h3>TEACHING AWARDS</h3>
 		</div>
 
 		<!-- Award Category -->
@@ -96,7 +113,7 @@
 					<h6>Name Surname</h6>
 					<p>Photo retrieved from SU ID of instructor</p>
 					<div class="dropdown">
-						<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Rank here
 						</button>
 						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -108,13 +125,64 @@
 				</div>
 			</div>
 			<!-- Repeat Card for other instructors -->
+            <div class="col-md-3 mb-4">
+				<div class="card">
+					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
+					<h6>Name Surname</h6>
+					<p>Photo retrieved from SU ID of instructor</p>
+					<div class="dropdown">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Rank here
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">1st place</a>
+							<a class="dropdown-item" href="#">2nd place</a>
+							<a class="dropdown-item" href="#">3rd place</a>
+						</div>
+					</div>
+				</div>
+			</div>
+            <div class="col-md-3 mb-4">
+				<div class="card">
+					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
+					<h6>Name Surname</h6>
+					<p>Photo retrieved from SU ID of instructor</p>
+					<div class="dropdown">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Rank here
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">1st place</a>
+							<a class="dropdown-item" href="#">2nd place</a>
+							<a class="dropdown-item" href="#">3rd place</a>
+						</div>
+					</div>
+				</div>
+			</div>
+            <div class="col-md-3 mb-4">
+				<div class="card">
+					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
+					<h6>Name Surname</h6>
+					<p>Photo retrieved from SU ID of instructor</p>
+					<div class="dropdown">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Rank here
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">1st place</a>
+							<a class="dropdown-item" href="#">2nd place</a>
+							<a class="dropdown-item" href="#">3rd place</a>
+						</div>
+					</div>
+				</div>
+			</div>
 			<div class="col-md-3 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
 					<p>Photo retrieved from SU ID of instructor</p>
 					<div class="dropdown">
-						<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Rank here
 						</button>
 						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -125,13 +193,13 @@
 					</div>
 				</div>
 			</div>
-            <div class="col-md-3 mb-4">
+			<div class="col-md-3 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
 					<p>Photo retrieved from SU ID of instructor</p>
 					<div class="dropdown">
-						<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Rank here
 						</button>
 						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -142,13 +210,30 @@
 					</div>
 				</div>
 			</div>
-            <div class="col-md-3 mb-4">
+			<div class="col-md-3 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
 					<p>Photo retrieved from SU ID of instructor</p>
 					<div class="dropdown">
-						<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Rank here
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">1st place</a>
+							<a class="dropdown-item" href="#">2nd place</a>
+							<a class="dropdown-item" href="#">3rd place</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 mb-4">
+				<div class="card">
+					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
+					<h6>Name Surname</h6>
+					<p>Photo retrieved from SU ID of instructor</p>
+					<div class="dropdown">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Rank here
 						</button>
 						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
