88260c38c608428bb4f936a3d5d3a8e4b2c04ca8
kayra ozerk <kayra.ozerk@sabanciuniv.edu>
2024-12-03 23:37:06 +0300

    Number of candidate per row increased to 6 from 4

--- voteScreen.php
+++ voteScreen.php
@@ -125,7 +125,7 @@
 		<!-- Cards -->
 		<div class="row">
 			<!-- Card Template -->
-			<div class="col-md-3 mb-4">
+			<div class="col-md-2 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
@@ -143,7 +143,7 @@
 				</div>
 			</div>
 			<!-- Repeat Card for other instructors -->
-            <div class="col-md-3 mb-4">
+            <div class="col-md-2 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
@@ -160,7 +160,7 @@
 					</div>
 				</div>
 			</div>
-            <div class="col-md-3 mb-4">
+            <div class="col-md-2 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
@@ -177,7 +177,7 @@
 					</div>
 				</div>
 			</div>
-            <div class="col-md-3 mb-4">
+            <div class="col-md-2 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
@@ -194,7 +194,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3 mb-4">
+			<div class="col-md-2 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
@@ -211,7 +211,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3 mb-4">
+			<div class="col-md-2 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
@@ -228,7 +228,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3 mb-4">
+			<div class="col-md-2 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
@@ -245,7 +245,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3 mb-4">
+			<div class="col-md-2 mb-4">
 				<div class="card">
 					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
 					<h6>Name Surname</h6>
@@ -262,6 +262,75 @@
 					</div>
 				</div>
 			</div>
+			<div class="col-md-2 mb-4">
+				<div class="card">
+					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
+					<h6>Name Surname</h6>
+					<p>Photo retrieved from SU ID of instructor</p>
+					<div class="dropdown">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Rank here
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">1st place</a>
+							<a class="dropdown-item" href="#">2nd place</a>
+							<a class="dropdown-item" href="#">3rd place</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2 mb-4">
+				<div class="card">
+					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
+					<h6>Name Surname</h6>
+					<p>Photo retrieved from SU ID of instructor</p>
+					<div class="dropdown">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Rank here
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">1st place</a>
+							<a class="dropdown-item" href="#">2nd place</a>
+							<a class="dropdown-item" href="#">3rd place</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2 mb-4">
+				<div class="card">
+					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
+					<h6>Name Surname</h6>
+					<p>Photo retrieved from SU ID of instructor</p>
+					<div class="dropdown">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Rank here
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">1st place</a>
+							<a class="dropdown-item" href="#">2nd place</a>
+							<a class="dropdown-item" href="#">3rd place</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2 mb-4">
+				<div class="card">
+					<img src="https://i.pinimg.com/originals/e7/13/89/e713898b573d71485de160a7c29b755d.png" alt="Instructor Photo">
+					<h6>Name Surname</h6>
+					<p>Photo retrieved from SU ID of instructor</p>
+					<div class="dropdown">
+						<button class="btn btn-indigo dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Rank here
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">1st place</a>
+							<a class="dropdown-item" href="#">2nd place</a>
+							<a class="dropdown-item" href="#">3rd place</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
 			<!-- Add more cards as needed -->
 		</div>
 	</div>
