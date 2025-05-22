367c6fde817e3d6a0d4cca3357c69134814c075e
Mustafa Yörükoğlu <mustafa.yorukoglu@sabanciuniv.edu>
2025-05-22 14:28:58 +0300

    Admin Yetki kontrolleri eklendi (impersonate, manageAcademicYear, authMiddleware, synchronizeFunc)

--- api/synchronizeFunc.php
+++ api/synchronizeFunc.php
@@ -8,6 +8,8 @@ ini_set('max_execution_time', 1200); // 20 minutes
 $config = require __DIR__ . '/../config.php';
 require_once '../database/dbConnection.php';
 
+enforceAdminAccess($pdo);
+
 header('Content-Type: application/json');
 
 
